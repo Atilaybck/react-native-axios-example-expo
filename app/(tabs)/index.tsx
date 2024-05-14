621001d73f0fdb31a8bafaae234be4c8baca7ffa
Atilay <atilaybck@gmail.com>
2024-05-14 19:57:18 +0300

    Değişiklikler yapıldı: X özelliği eklendi

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,57 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  Text,
+  ActivityIndicator,
+  StyleSheet,
+  View,
+  Button,
+  FlatList,
+} from "react-native";
+import axios from "axios";
+import UserCard from "@/components/UserCard";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const URL = "https://jsonplaceholder.typicode.com/users";
 
-export default function HomeScreen() {
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [userList, setUserList] = useState([]);
+
+  async function fetchData() {
+    const response = await axios.get(URL);
+    setLoading(false);
+    setUserList(response.data);
+    console.log(response);
+    // response.data, JSONPlaceholder API'den TÜM dönen kullanıcı verisi içerir.
+    //flatlist örneklerindeki DATA gibi düşün.
+  }
+
+  const renderUser = ({ item }) => (
+    <UserCard name={item.name} email={item.email} userName={item.userName} />
+  );
+
+  useEffect(() => {
+    fetchData()
+  }, []);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <View>
+        {loading ? (
+          <ActivityIndicator size="large" />
+        ) : (
+          <FlatList data={userList} renderItem={renderUser} />
+        )}
+      </View>
+    </View>
   );
 }
 
+export default App;
+
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  container: {
+    flex: 1,
+    marginTop: 30,
   },
 });
